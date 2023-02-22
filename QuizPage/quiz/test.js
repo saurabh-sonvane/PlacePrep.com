a31ec114c83917b1fdf63ef06a8b183f52666287
Himanshu Sharma <h9914246367@gmail.com>
2023-02-23 04:20:46 +0530

    quiz-page

---
+++ QuizPage/quiz/test.js
@@ -0,0 +1,44 @@
+let quizzes = [
+    {
+        "question": " Which one of the following node is considered the top of the stack if the stack is implemented using the linked list?",
+        "optionA": "First node",
+        "optionB": "Second node",
+        "optionC": "Last node",
+        "optionD": "None of the above",
+        "multipleAnswers": "false",
+        "answerA": "true",
+        "answerB": "false",
+        "answerC": "false",
+        "answerD": "false",
+        "category": "dsa",
+        "id": 60
+    },
+    {
+        "question": " Which one of the following node is considered the top of the stack if the stack is implemented using the linked list?",
+        "optionA": "First node",
+        "optionB": "Second node",
+        "optionC": "Last node",
+        "optionD": "None of the above",
+        "multipleAnswers": "false",
+        "answerA": "true",
+        "answerB": "false",
+        "answerC": "false",
+        "answerD": "false",
+        "category": "dsa",
+        "id": 60
+    },
+    {
+        "question": " Which one of the following node is considered the top of the stack if the stack is implemented using the linked list?",
+        "optionA": "First node",
+        "optionB": "Second node",
+        "optionC": "Last node",
+        "optionD": "None of the above",
+        "multipleAnswers": "false",
+        "answerA": "true",
+        "answerB": "false",
+        "answerC": "false",
+        "answerD": "false",
+        "category": "dsa",
+        "id": 60
+    }
+]
