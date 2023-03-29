b141778cb9d3b262f8fb9c127c088ef00122e0a4
Himanshu Sharma <h9914246367@gmail.com>
2023-03-29 08:31:19 +0530

    opt done

--- login/loginSignup.js
+++ login/loginSignup.js
@@ -4,7 +4,11 @@ const loginBtn = document.getElementById('login');
 const signupBtn = document.getElementById('signup');
 const loginSubmitBtn = document.getElementById('login-btn');
 const signupSubmitBtn = document.getElementById('signup-btn');
-
+const optBtn = document.getElementById('otp-continue');
+const optCloseBtn = document.getElementById('otp-close-btn');
+window.onload = () => {
+    emailjs.init("a0ZPOjoa8Y8snS_y8");
+}
 loginBtn.addEventListener('click', (e) => {
     let parent = e.target.parentNode.parentNode;
     Array.from(e.target.parentNode.parentNode.classList).find((element) => {
@@ -33,9 +37,15 @@ signupSubmitBtn.addEventListener('click', (e) => {
     e.preventDefault();
     let name = document.getElementById('user-name').value;
     let email = document.getElementById('user-email').value;
-    let password = document.getElementById('user-password').value;
-    console.log(name, email, password);
-    addUser({ body: JSON.stringify({ name: name, email: email, password: password, totalScore: 0, quizScores: {} }) })
+    let otpToSend = Math.floor(1000 + Math.random() * 9000);
+    localStorage.setItem('otp', otpToSend);
+    emailjs.send("service_co7vkub", "template_o8haspq", { from_name: 'PlacePrep', to_name: name, to_email: email, the_otp: otpToSend, reply_to: email })
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+        }, function (error) {
+            console.log('FAILED...', error);
+        });
+    $('#otpModal').modal({ show: true, backdrop: 'static' })
 })
 loginSubmitBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -44,6 +54,27 @@ loginSubmitBtn.addEventListener('click', (e) => {
     console.log(email, password);
     getUser({ email, password })
 })
+optBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let name = document.getElementById('user-name').value;
+    let email = document.getElementById('user-email').value;
+    let password = document.getElementById('user-password').value;
+    console.log(name, email, password);
+    let otpInput = document.getElementById('otp-input').value;
+    let otp = localStorage.getItem('otp');
+    console.log(otp, otpInput, otp == otpInput);
+    if (otp == otpInput) {
+        verifyOtp(name, email, password)
+    }
+})
+
+function verifyOtp(name, email, password) {
+
+    addUser({ body: JSON.stringify({ name: name, email: email, password: password, totalScore: 0, quizScores: {} }) })
+    optCloseBtn.click();
+    loginBtn.click();
+}
+
 async function addUser({ body }) {
     console.log(body);
     try {
@@ -55,7 +86,6 @@ async function addUser({ body }) {
             body: body,
         });
         console.log("res", res);
-        loginBtn.click();
     } catch (error) {
         console.log("error", error);
     }
